aa051b8931f97b5007ad4e27593f1ccaeaa2cc3d
Stephen <stephen.s.espinosa@gmail.com>
2023-07-14 22:14:00 +0200

    modify js add css styles

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const secretNumber = Math.trunc(Math.random() * 20) + 1;
 const scoreBoard = document.querySelector(".label-score span");
 let score = 20;
 
+console.log(secretNumber);
+
 scoreBoard.textContent = score;
 
 document.querySelector(".check").addEventListener("click", function () {
@@ -12,15 +14,21 @@ document.querySelector(".check").addEventListener("click", function () {
     document.querySelector(".message").textContent =
       "No Number! Please type a number between 1-20.";
   } else if (guess === secretNumber) {
+    document.querySelector("body").style.backgroundColor = "#60b347";
+
+    document.querySelector(".number").style.width = "30rem";
     document.querySelector(".number").textContent = secretNumber;
+
     document.querySelector(".message").textContent =
       "You have guess the correct number!";
   } else if (guess < secretNumber) {
     score--;
+
     document.querySelector(".message").textContent =
       "Your number is lower than the secret number, try again.";
   } else if (guess > secretNumber) {
     score--;
+
     document.querySelector(".message").textContent =
       "Your number is higher than the secret number, try again.";
   }
@@ -32,6 +40,9 @@ document.querySelector(".check").addEventListener("click", function () {
 function checkScore(score) {
   if (score < 1) {
     scoreBoard.textContent = 0;
+
+    document.querySelector("body").style.backgroundColor = "red";
+
     document.querySelector(".message").textContent =
       "You lose the game. Try again!";
   }
