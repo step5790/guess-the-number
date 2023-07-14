9ca512822e6036fa42b0e2f6cd4236db13dbe5b3
Stephen <stephen.s.espinosa@gmail.com>
2023-07-14 21:29:47 +0200

    modify js add click event

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-'use strict';
+"use strict";
+
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".guess").value);
+  if (!guess) {
+    document.querySelector(".message").textContent =
+      "No Number! Please type a number between 1-20";
+  }
+});
